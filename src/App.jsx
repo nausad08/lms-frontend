c67dd3735408d471d95d9ff9daaa29b84045a737
nausad <na2439@it.jgec.ac.in>
2024-03-13 15:22:43 +0530

    tailwind configaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-        <h1>hii boss</h1>
+        <h1 className='text-red-400'>hii boss</h1>
     </>
   )
 }
